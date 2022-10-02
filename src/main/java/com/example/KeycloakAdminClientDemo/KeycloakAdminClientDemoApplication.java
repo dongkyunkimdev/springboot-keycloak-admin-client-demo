12983d22e35f26ee425da348c9244366eebf759d
dongkyunkimdev <ddkds66@gmail.com>
2022-10-02 16:49:51 +0900

    Set the spring boot application

---
+++ src/main/java/com/example/KeycloakAdminClientDemo/KeycloakAdminClientDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.KeycloakAdminClientDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakAdminClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakAdminClientDemoApplication.class, args);
+	}
+
+}
